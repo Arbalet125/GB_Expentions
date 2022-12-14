c635af4c50912298fc4ae300718e08512f5c1cd8
Arbalet <MametovV@gmail.com>
2022-12-14 14:06:05 +0300

    last task complete

--- second_seminar/dz_2_seminar/main.java
+++ second_seminar/dz_2_seminar/main.java
@@ -103,3 +103,27 @@ public static void printSum(Integer a, Integer b){
 } */
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. Пользователю должно /////////
+// показаться сообщение, что пустые строки вводить нельзя.///////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/* import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main {
+    public static void nullString(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите текст (пустую строку вводить нельзя)");
+        try {
+            String result = reader.readLine();
+            if(result.equals("")) throw new RuntimeException("Вы ввели пустую строку...");
+
+            System.out.println(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+} */
